d1fb42e4d0ce55d3dce2674b1524362eeaac8234
hjlzg <hjlzg@outlook.com>
2020-11-23 08:26:06 +0800

    add go-gin-example

---
+++ go-gin-example/pkg/setting/setting.go
@@ -0,0 +1,68 @@
+package setting
+
+import (
+	"log"
+	"time"
+
+	"github.com/go-ini/ini"
+)
+
+var (
+	// Cfg is
+	Cfg *ini.File
+
+	//RunMode is
+	RunMode string
+
+	//HTTPPort is
+	HTTPPort int
+	//ReadTimeout is
+	ReadTimeout time.Duration
+	//WriteTimeout is
+	WriteTimeout time.Duration
+
+	//PageSize is
+	PageSize int
+	//JwtSecret is
+	JwtSecret string
+)
+
+func init() {
+	var err error
+	Cfg, err = ini.Load("conf/app.ini")
+	if err != nil {
+		log.Fatalf("Fail to parse 'conf/app.ini': %v", err)
+	}
+
+	LoadBase()
+	LoadServer()
+	LoadApp()
+}
+
+// LoadBase is
+func LoadBase() {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
+
+// LoadServer is
+func LoadServer() {
+	sec, err := Cfg.GetSection("server")
+	if err != nil {
+		log.Fatalf("Fail to get section 'server': %v", err)
+	}
+
+	HTTPPort = sec.Key("HTTP_PORT").MustInt(8000)
+	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60)) * time.Second
+	WriteTimeout = time.Duration(sec.Key("WRITE_TIMEOUT").MustInt(60)) * time.Second
+}
+
+// LoadApp is
+func LoadApp() {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatalf("Fail to get section 'app': %v", err)
+	}
+
+	JwtSecret = sec.Key("JwtSecret").MustString("!@)*#)!@U#@*!@!)")
+	PageSize = sec.Key("PageSize").MustInt(10)
+}
--- go-gin-example/readme.md
+++ go-gin-example/readme.md
@@ -1,2 +1,12 @@
 ### gin学习系列
 * [first](https://eddycjy.com/posts/go/gin/2018-02-10-install/)
+
+``` 目录结构
+go-gin-example
+├── conf 用于存储配置文件
+├── middleware 应用中间件
+├── models 应用数据库模型
+├── pkg 第三方包
+├── routers 路由逻辑处理
+└── runtime 应用运行时数据
+```
